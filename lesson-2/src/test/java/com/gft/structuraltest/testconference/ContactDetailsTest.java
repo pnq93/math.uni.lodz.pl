83db1a397d74de494979b5f2f7be0ad2da9fb24b
Kamil <pnq.priv@gmail.com>
2015-11-12 12:07:34 +0100

    lesson2_homework

--- lesson-2/src/test/java/com/gft/structuraltest/testconference/ContactDetailsTest.java
+++ lesson-2/src/test/java/com/gft/structuraltest/testconference/ContactDetailsTest.java
@@ -15,10 +15,74 @@ public class ContactDetailsTest {
     @Test
     @Parameters({
             "jacek@wp.pl,false",
-            "jacek,true"
+            "jacek,true",
+            "<script></script>,false",
+            "'',false",
+            "*,false",
+            "\\n,false",
+            "OR '1' = '1' ,false",
+            "+,false",
+            "<html></html>,false",
+            "atyhythtyIJOHJFEJKNKJIO,false",
+            "12345,false",
+            "abdds1212,false",
+            "@dasda,false",
+            "dsadasd dasdasd,false",
+            " dsadfds,false",
+            "aęęŻÓ,true",
+            "kdiwĘżcofiIDMPćężąa,true",
+            "aKSąćKIJFOjfjdsfsdidf,false"
     })
     public void shouldReturnTrueIfFirstNameIsValid(String firstName, boolean result)  {
         assertEquals(ContactDetails.validateFirstName(firstName), result);
     }
+
+    @Test
+    @Parameters({
+    		"jacek@wp.pl,false",
+    		"523587523,true",
+    		"234,true",
+    		"9632884221633,true",
+    		"asaADdsa,false",
+    		"     585566222,true",
+    		"324 588 325,true",
+    		"@852482,false",
+    		"<html></html>,false",
+    		"<script></script>,false",
+            "'',false",
+            "*,false",
+            "\\n,false",
+            "OR '1' = '1' ,false",
+            "^22133,false"
+    })
+    public void shouldReturnTrueIfPhoneNumberIsValid(String phoneNumber, boolean result){
+    	assertEquals(ContactDetails.validatePhone(phoneNumber), result);
+    }
+
+    @Test
+    @Parameters({
+    	"jacek@wp.pl,true",
+    	" dbe@o2.pl,false",
+		"56565@wp.pl,true",
+		"523587523,false",
+		"234,false",
+		"9632884221633,false",
+		"asa48@gmail.com,true",
+		"@852482,false",
+		"<html></html>,false",
+		"<script></script>,false",
+        "'',false",
+        "*,false",
+        "\\n,false",
+       "OR '1' = '1' ,false",
+        "kapuastajestdobra.pl,false"
+    })
+    public void shouldReturnTrueIfEmailIsValid(String email, boolean result){
+    	assertEquals(ContactDetails.validateEmail(email), result);
+    }
+
+
+
+
 }
 
