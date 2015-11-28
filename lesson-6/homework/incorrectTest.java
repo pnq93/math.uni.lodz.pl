f6a59cdaa03b03d5e13bd8429470695d993f03d1
Kamil <pnq.priv@gmail.com>
2015-11-28 01:07:31 +0100

    lesson-6_homework

---
+++ lesson-6/homework/incorrectTest.java
@@ -0,0 +1,84 @@
+package com.example.tests;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class IncorrectTest {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @Before
+  public void setUp() throws Exception {
+    driver = new FirefoxDriver();
+    baseUrl = "http://localhost:8080/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testIncorrect() throws Exception {
+    driver.get(baseUrl + "/registrationform/");
+    driver.findElement(By.name("firstName")).clear();
+    driver.findElement(By.name("firstName")).sendKeys("Robert");
+    driver.findElement(By.name("lastName")).clear();
+    driver.findElement(By.name("lastName")).sendKeys("Kowal");
+    driver.findElement(By.name("email")).clear();
+    driver.findElement(By.name("email")).sendKeys("kowal@o2.pl");
+    driver.findElement(By.name("repeatEmail")).clear();
+    driver.findElement(By.name("repeatEmail")).sendKeys("kowal@o2.pl");
+    driver.findElement(By.cssSelector("button.gwt-Button")).click();
+    driver.findElement(By.id("gwt-uid-22")).click();
+    driver.findElement(By.id("gwt-uid-26")).click();
+    driver.findElement(By.cssSelector("table.form-buttons > tbody > tr > td > button.gwt-Button")).click();
+    driver.findElement(By.xpath("(//button[@type='button'])[6]")).click();
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
