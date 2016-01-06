ffadd691ee9462205e145a822c172bebc01c46c8
Kamil <pnq.priv@gmail.com>
2016-01-06 15:28:36 +0100

    homework_8

--- lesson-8/src/test/java/SimpleTest.java
+++ lesson-8/src/test/java/SimpleTest.java
@@ -1,3 +1,4 @@
+
 /**
  * Created by Jacek on 2015-08-01.
  */
@@ -12,34 +13,96 @@ import static org.junit.Assert.*;
 
 public class SimpleTest {
 
-    private static WebDriver driver;
+	private static WebDriver driver;
 
-    private static final String URL = "http://localhost:8080/registrationform/";
+	private static final String URL = "http://localhost:8080/registrationform/";
 
-    @Before
-    public  void openBrowser(){
-        driver = new FirefoxDriver();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	@Before
+	public void openBrowser() {
+		driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
-    }
+	}
 
-    @Test
-    public  void shouldShowWrongAnswerPopupWhenAsnwerIsIncorrect(){
+	@Test
+	public void shouldShowWrongAnswerPopupWhenAsnwerIsIncorrect() {
 
-        driver.get(URL);
-        String title = driver.getTitle();
-        assertEquals("Rule Financial Registration Form", title);
+		driver.get(URL);
+		String title = driver.getTitle();
+		assertEquals("Rule Financial Registration Form", title);
 
-        /*
-         * Your code needs to be here, sample bolow should help you a bit
-         * driver.findElement(By.name("firstName")).clear();
-         * driver.findElement(By.name("firstName")).sendKeys("Marek");
-         */
+		driver.findElement(By.name("firstName")).clear();
+		driver.findElement(By.name("firstName")).sendKeys("Kamil");
+		driver.findElement(By.name("lastName")).clear();
+		driver.findElement(By.name("lastName")).sendKeys("Pawelec");
 
-    }
+		String email = new String("sdfsdf3rg45g4g4gdgfdg.pl");
+		driver.findElement(By.name("email")).clear();
+		driver.findElement(By.name("email")).sendKeys(email);
+		driver.findElement(By.name("repeatEmail")).clear();
+		driver.findElement(By.name("repeatEmail")).sendKeys(email);
+		driver.findElement(By.xpath("//*[@class='gwt-Button' and text()='SQL']")).click();
+		driver.findElement(By.xpath("//*[@id='gwt-uid-22']")).click();
+		driver.findElement(By.xpath("//*[@id='gwt-uid-26']")).click();
+		assertEquals(checkAnswer(), false);
+	}
 
-    @After
-    public void closeBrowser(){
-        driver.quit();
-    }
+	@Test
+	public void shouldShowWrongAnswerPopupWhenAsnwerIsCorrect() {
+
+		driver.get(URL);
+		String title = driver.getTitle();
+		assertEquals("Rule Financial Registration Form", title);
+
+		driver.findElement(By.name("firstName")).clear();
+		driver.findElement(By.name("firstName")).sendKeys("Kamil");
+		driver.findElement(By.name("lastName")).clear();
+		driver.findElement(By.name("lastName")).sendKeys("Pawelec");
+
+		String email = new String("pnq.priv@gmail.com");
+		driver.findElement(By.name("email")).clear();
+		driver.findElement(By.name("email")).sendKeys(email);
+		driver.findElement(By.name("repeatEmail")).clear();
+		driver.findElement(By.name("repeatEmail")).sendKeys(email);
+		driver.findElement(By.xpath("//*[@class='gwt-Button' and text()='SQL']")).click();
+		driver.findElement(By.xpath("//*[@id='gwt-uid-23']")).click();
+		driver.findElement(By.xpath("//*[@id='gwt-uid-26']")).click();
+		assertEquals(checkAnswer(), true);
+	}
+
+	private Object checkAnswer() {
+		if (driver.findElement(By.xpath("//*[@class='GOFRIDJCEI' and text()='Congratulations! You've won:']"))
+				.isDisplayed())
+			return true;
+		else
+			return false;
+	}
+
+	@Test
+	public void shouldNotifyWhenRepeatedEmailIsDifferentFromYourEmail() {
+		driver.get(URL);
+		String title = driver.getTitle();
+		assertEquals("Rule Financial Registration Form", title);
+
+		driver.findElement(By.name("firstName")).clear();
+		driver.findElement(By.name("firstName")).sendKeys("Kamil");
+		driver.findElement(By.name("lastName")).clear();
+		driver.findElement(By.name("lastName")).sendKeys("Pawelec");
+
+		String email = new String("pnq.priv@gmail.com");
+		String emailRepeat = new String("pnq.priv93@gmail.com");
+		driver.findElement(By.name("email")).clear();
+		driver.findElement(By.name("email")).sendKeys(email);
+
+		driver.findElement(By.name("repeatEmail")).clear();
+		driver.findElement(By.name("repeatEmail")).sendKeys(emailRepeat);
+
+		assertEquals("Please provide an identical email as you have written above.",
+				driver.findElement(By.xpath("html/body/div[2]/div[2]/div/div/div/div[5]/div/div")).getText());
+	}
+
+	@After
+	public void closeBrowser() {
+		driver.quit();
+	}
 }
